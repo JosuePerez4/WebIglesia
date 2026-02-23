bfedfe653566a0c584b9b9217bae3e46431fdb81
Josué Pérez <josuedanielpg@ufps.edu.co>
2026-02-22 20:08:55 -0500

    Inicio de este lindo proyecto

---
+++ src/main/java/icc/sanluis/webiglesia/IccSanLuisApplication.java
@@ -0,0 +1,13 @@
+package icc.sanluis.webiglesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IccSanLuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IccSanLuisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/icc/sanluis/webiglesia/IccSanLuisApplicationTests.java
@@ -0,0 +1,13 @@
+package icc.sanluis.webiglesia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IccSanLuisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
